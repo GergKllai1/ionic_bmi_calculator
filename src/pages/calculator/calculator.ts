7f545cb81277d428fc2abdd3f9a70022d89657b1
Gregory Kallai <gerg.kallai@gmail.com>
2018-12-10 14:46:06 +0100

    Adds BMI calculation logic to calculator ts, puts the calculator html inside ion content thus correcting an error

--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -7,8 +7,36 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'calculator.html',
 })
 export class CalculatorPage {
+  height: number;
+  weight: number;
+  bmiValue: number;
+  bmiMessage: string;
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  calculateBMI(){
+    if( this.weight > 0 && this.height > 0 ) {
+      let calculation = this.weight / ( this.height / 100 * this.height / 100 );
+      this.bmiValue = parseFloat( calculation.toFixed( 2 ) );
+      this.setBMIMessage();
+    }
+  }
+
+  private setBMIMessage() {
+    if ( this.bmiValue < 18.5 ) {
+      this.bmiMessage = "Underweight"
+    }
+    else if ( this.bmiValue > 18.5 && this.bmiValue < 25 ) {
+      this.bmiMessage = "Normal weight"
+    }
+    else if ( this.bmiValue > 25 ) {
+      this.bmiMessage = "Overweight"
+    }
+    else if ( this.bmiValue > 30 ) {
+      this.bmiMessage = "Obese"
+    }
   }
 
   ionViewDidLoad() {
